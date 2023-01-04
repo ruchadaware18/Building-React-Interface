87c82955fa5e490f03526734d923da55c3da2999
Rucha Daware <ruchad18@gmail.com>
2023-01-04 18:11:42 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BiArchive } from "react-icons/bi";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <BiArchive />
+        Your Appointments
+      </h1>
+    </div>
+  );
+}
+
+export default App;
